09cfa9e100e8d5bddf92691b5e149247a99a4bd1
Kan <Kanpengnet@gmai.com>
2016-08-01 14:29:56 +0700

    Desigh UI SpashScreen

--- app/src/main/java/kbu/sineepun/kanpengnet/badmintontraining/SplashScreen.java
+++ app/src/main/java/kbu/sineepun/kanpengnet/badmintontraining/SplashScreen.java
@@ -2,13 +2,27 @@ package kbu.sineepun.kanpengnet.badmintontraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
-
+    //Explicit
+    private ImageView imageView;
+    private TextView team1TextView, team2TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+        //bind widget
+        imageView = (ImageView) findViewById(R.id.imageView);
+        team1TextView = (TextView) findViewById(R.id.textView2);
+        team2TextView = (TextView) findViewById(R.id.textView3);
+        //show view
+        MyConfig myConfig = new MyConfig();
+        imageView.setImageResource(myConfig.getLogoAnInt());
+        team1TextView.setText(myConfig.getTeam1String());
+        team2TextView.setText(myConfig.getTeam2String());
+
+    }//main Method
+}//main Class
