4d22a46a8015e0c3a87d99dfe87296aed70b3367
Kan <Kanpengnet@gmai.com>
2016-08-01 16:33:34 +0700

    Back Videofinish

--- app/src/main/java/kbu/sineepun/kanpengnet/badmintontraining/VideoActivity.java
+++ app/src/main/java/kbu/sineepun/kanpengnet/badmintontraining/VideoActivity.java
@@ -2,6 +2,8 @@ package kbu.sineepun.kanpengnet.badmintontraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-    }
-}
+
+        //Active Back
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }//main method
+}//main Class
